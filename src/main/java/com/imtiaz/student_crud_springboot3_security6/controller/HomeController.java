7c7217bf7be9c32eb2591def7c202aca809a02cc
Md Imtiaz Hossain <55924609+Md-Imtiaz-Hossain@users.noreply.github.com>
2023-03-16 00:53:36 +0600

    Refactor

--- src/main/java/com/imtiaz/student_crud_springboot3_security6/controller/HomeController.java
+++ src/main/java/com/imtiaz/student_crud_springboot3_security6/controller/HomeController.java
@@ -1,7 +1,7 @@
 package com.imtiaz.student_crud_springboot3_security6.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
